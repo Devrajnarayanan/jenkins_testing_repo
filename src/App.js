abb9f4bddaf02d5a00a43d97331ef65bbbb8154c
Devrajnarayanan <113659584+Devrajnarayanan@users.noreply.github.com>
2024-08-06 10:43:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.Deployed using Jenkins Pipeline Successfully-Auto2-Deployed by Main branch.
+          Edit <code>src/App.js</code> and save to reload.Deployed using Jenkins Pipeline Successfully-Auto2-Deployed by Main branch v1.
         </p>
         <a
           className="App-link"
